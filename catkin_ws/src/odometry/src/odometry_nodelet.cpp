bdf87756d0a7e1778a85070a8418ab065332ebc1
Koichi Hikawa <rkoichi2001@gmail.com>
2019-08-19 00:07:01 +0900

    gmapping worked.

--- catkin_ws/src/odometry/src/odometry_nodelet.cpp
+++ catkin_ws/src/odometry/src/odometry_nodelet.cpp
@@ -1,8 +1,8 @@
 
 
 //
-#include <sys/time.h>
 #include <cstddef>
+#include <sys/time.h>
 
 // ROS
 #include <message_filters/subscriber.h>
@@ -12,18 +12,16 @@
 #include <nodelet/nodelet.h>
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
+#include <string>
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_datatypes.h>
-#include <string>
 
 // 3rd party include
 #include "kmsgs/Imu.h"
 #include "odometry.h"
 
-namespace koichi_robotics_lib
-{
-class OdometryNodelet : public nodelet::Nodelet
-{
+namespace koichi_robotics_lib {
+class OdometryNodelet : public nodelet::Nodelet {
 public:
   OdometryNodelet();
 
@@ -32,11 +30,13 @@ public:
   virtual void onInit();
 
 private:
-  void publishTransform(const nav_msgs::Odometry &odomMsg, std::string frameName);
+  void publishTransform(const nav_msgs::Odometry &odomMsg,
+                        std::string frameName);
 
   void sensorCallback(const sensor_msgs::JointStateConstPtr &jointPos);
 
-  void sensorCallback(const kmsgs::ImuConstPtr &imuMsg, const sensor_msgs::JointStateConstPtr &jointPos);
+  void sensorCallback(const kmsgs::ImuConstPtr &imuMsg,
+                      const sensor_msgs::JointStateConstPtr &jointPos);
 
   void calcAndPublishOdom(const ros::Time &stamp);
 
@@ -44,15 +44,18 @@ private:
 
   void readParam(std::string paramName, bool result);
 
-  void parseJointStatesMsg(const sensor_msgs::JointStateConstPtr &jointPos, double &rRad, double &lRad, double &rVel,
+  void parseJointStatesMsg(const sensor_msgs::JointStateConstPtr &jointPos,
+                           double &rRad, double &lRad, double &rVel,
                            double &lVel);
 
-  void fillOdomMessage(nav_msgs::Odometry &odomMsg, std::string frameName, const ros::Time &stamp,
-                       const OdomPos &odomPos, const OdomVel &odomVel);
+  void fillOdomMessage(nav_msgs::Odometry &odomMsg, std::string frameName,
+                       const ros::Time &stamp, const OdomPos &odomPos,
+                       const OdomVel &odomVel);
 
 private:
   // Topic Name
-  std::string imuTopicName, jointStatesTopicName, odomWhlTopicName, odomImuTopicName;
+  std::string imuTopicName, jointStatesTopicName, odomWhlTopicName,
+      odomImuTopicName;
   std::string odomImuFrameName, odomWhlFrameName, baselinkFrameName;
 
   int maxSyncDiff;
@@ -67,7 +70,9 @@ private:
   message_filters::Subscriber<sensor_msgs::JointState> *subWhl;
 
   // ROS Sync Policy
-  typedef message_filters::sync_policies::ApproximateTime<kmsgs::Imu, sensor_msgs::JointState> OdomSyncPolicy;
+  typedef message_filters::sync_policies::ApproximateTime<
+      kmsgs::Imu, sensor_msgs::JointState>
+      OdomSyncPolicy;
   message_filters::Synchronizer<OdomSyncPolicy> *odomSync;
 
   // ROS Publisher
@@ -77,17 +82,14 @@ private:
   // Class
   Odometry odometry;
 };
-}
+} // namespace koichi_robotics_lib
 
 using namespace std;
 using namespace koichi_robotics_lib;
 
-OdometryNodelet::OdometryNodelet()
-{
-}
+OdometryNodelet::OdometryNodelet() {}
 
-OdometryNodelet::~OdometryNodelet()
-{
+OdometryNodelet::~OdometryNodelet() {
   odometry.Finalize();
   delete this->odomBroadCaster;
   delete this->subImu;
@@ -95,8 +97,7 @@ OdometryNodelet::~OdometryNodelet()
   delete this->odomSync;
 }
 
-void OdometryNodelet::onInit()
-{
+void OdometryNodelet::onInit() {
   NODELET_DEBUG("Initializing Odometry Nodelet....");
 
   nh = getMTNodeHandle();
@@ -107,87 +108,124 @@ void OdometryNodelet::onInit()
   readParams(params);
   odometry.Initialize(params);
 
+  /*
   subImu = new message_filters::Subscriber<kmsgs::Imu>(nh, imuTopicName, 100);
-  subWhl = new message_filters::Subscriber<sensor_msgs::JointState>(nh, jointStatesTopicName, 100);
+  subWhl = new message_filters::Subscriber<sensor_msgs::JointState>(
+      nh, jointStatesTopicName, 100);
+  */
 
-  if (odom_whl_only)
-  {
+  subImu = new message_filters::Subscriber<kmsgs::Imu>(nh, "imu", 100);
+  subWhl = new message_filters::Subscriber<sensor_msgs::JointState>(
+      nh, "joint_states", 100);
+
+  if (odom_whl_only) {
     subWhl->registerCallback(&OdometryNodelet::sensorCallback, this);
-  }
-  else
-  {
-    odomSync = new message_filters::Synchronizer<OdomSyncPolicy>(OdomSyncPolicy(maxSyncDiff), *subImu, *subWhl);
-    odomSync->registerCallback(boost::bind(&OdometryNodelet::sensorCallback, this, _1, _2));
+  } else {
+    odomSync = new message_filters::Synchronizer<OdomSyncPolicy>(
+        OdomSyncPolicy(maxSyncDiff), *subImu, *subWhl);
+    odomSync->registerCallback(
+        boost::bind(&OdometryNodelet::sensorCallback, this, _1, _2));
   }
 
-  odomPubWhl = nh.advertise<nav_msgs::Odometry>(odomWhlTopicName, 100);
-  odomPubImu = nh.advertise<nav_msgs::Odometry>(odomImuTopicName, 100);
+  // odomPubWhl = nh.advertise<nav_msgs::Odometry>(odomWhlTopicName, 100);
+  // odomPubImu = nh.advertise<nav_msgs::Odometry>(odomImuTopicName, 100);
+  odomPubWhl = nh.advertise<nav_msgs::Odometry>("odom_whl", 100);
+  odomPubImu = nh.advertise<nav_msgs::Odometry>("odom_imu", 100);
 }
 
-void OdometryNodelet::readParams(OdomParams &odomParams)
-{
+void OdometryNodelet::readParams(OdomParams &odomParams) {
   // Topic Name
-  readParam("imu_topic_name", nh_ns.param<std::string>("imu_topic_name", imuTopicName, ""));
-  readParam("joint_states_topic_name", nh_ns.param<std::string>("joint_states_topic_name", jointStatesTopicName, ""));
-  readParam("odom_whl_topic_name", nh_ns.param<std::string>("odom_whl_topic_name", odomWhlTopicName, ""));
-  readParam("odom_imu_topic_name", nh_ns.param<std::string>("odom_imu_topic_name", odomImuTopicName, ""));
+  /**
+  readParam("imu_topic_name",
+            nh_ns.param<std::string>("imu_topic_name", imuTopicName, ""));
+  readParam("joint_states_topic_name",
+            nh_ns.param<std::string>("joint_states_topic_name",
+                                     jointStatesTopicName, ""));
+  readParam(
+      "odom_whl_topic_name",
+      nh_ns.param<std::string>("odom_whl_topic_name", odomWhlTopicName, ""));
+  readParam(
+      "odom_imu_topic_name",
+      nh_ns.param<std::string>("odom_imu_topic_name", odomImuTopicName, ""));
+  **/
 
   // Frame Name
-  readParam("odometry_imu_frame_name", nh_ns.param<std::string>("odometry_imu_frame_name", odomImuFrameName, ""));
-  readParam("odometry_whl_frame_name", nh_ns.param<std::string>("odometry_whl_frame_name", odomWhlFrameName, ""));
-  readParam("base_link_frame_name", nh_ns.param<std::string>("base_link_frame_name", baselinkFrameName, ""));
+  readParam("odometry_imu_frame_name",
+            nh_ns.param<std::string>("odometry_imu_frame_name",
+                                     odomImuFrameName, ""));
+  readParam("odometry_whl_frame_name",
+            nh_ns.param<std::string>("odometry_whl_frame_name",
+                                     odomWhlFrameName, ""));
+  readParam(
+      "base_link_frame_name",
+      nh_ns.param<std::string>("base_link_frame_name", baselinkFrameName, ""));
 
   // Hardware Related
-  readParam("gyro_1bit_in_rad", nh_ns.param<double>("gyro_1bit_in_rad", odomParams.gyro1BitInRad, 0.0));
+  readParam(
+      "gyro_1bit_in_rad",
+      nh_ns.param<double>("gyro_1bit_in_rad", odomParams.gyro1BitInRad, 0.0));
   double r_whl_dia, l_whl_dia;
-  readParam("l_whl_diameter", nh_ns.param<double>("l_whl_diameter", l_whl_dia, 0.0));
+  readParam("l_whl_diameter",
+            nh_ns.param<double>("l_whl_diameter", l_whl_dia, 0.0));
   odomParams.leftDistPerRad = l_whl_dia;
-  readParam("r_whl_diameter", nh_ns.param<double>("r_whl_diameter", r_whl_dia, 0.0));
+  readParam("r_whl_diameter",
+            nh_ns.param<double>("r_whl_diameter", r_whl_dia, 0.0));
   odomParams.rightDistPerRad = r_whl_dia;
-  readParam("wheel_base", nh_ns.param<double>("wheel_base", odomParams.wheelBase, 1.0));
+  readParam("wheel_base",
+            nh_ns.param<double>("wheel_base", odomParams.wheelBase, 1.0));
 
   // Odometry Parameter
-  readParam("standstill_thresh", nh_ns.param<double>("standstill_thresh", odomParams.standstillThresh, 0.0));
-  readParam("use_left_pls", nh_ns.param<bool>("use_left_pls", odomParams.useLeftPls, false));
-  readParam("use_right_pls", nh_ns.param<bool>("use_right_pls", odomParams.useRightPls, false));
-  readParam("cycle_time", nh_ns.param<double>("cycle_time", odomParams.cycleTime, 0.0));
+  readParam("standstill_thresh",
+            nh_ns.param<double>("standstill_thresh",
+                                odomParams.standstillThresh, 0.0));
+  readParam("use_left_pls",
+            nh_ns.param<bool>("use_left_pls", odomParams.useLeftPls, false));
+  readParam("use_right_pls",
+            nh_ns.param<bool>("use_right_pls", odomParams.useRightPls, false));
+  readParam("cycle_time",
+            nh_ns.param<double>("cycle_time", odomParams.cycleTime, 0.0));
   cycle_time = odomParams.cycleTime;
   readParam("max_sync_diff", nh_ns.param<int>("max_sync_diff", maxSyncDiff, 8));
-  readParam("odom_whl_only", nh_ns.param<bool>("odom_whl_only", odom_whl_only, false));
+  readParam("odom_whl_only",
+            nh_ns.param<bool>("odom_whl_only", odom_whl_only, false));
 
   // Buffer Size
-  readParam("imu_yaw_buffer", nh_ns.param<int>("imu_yaw_buffer", odomParams.imuYawBufferSize, 0.0));
-  readParam("whl_yaw_buffer", nh_ns.param<int>("whl_yaw_buffer", odomParams.whlYawBufferSize, 0.0));
-  readParam("dist_buffer", nh_ns.param<int>("dist_buffer", odomParams.distBufferSize, 0));
+  readParam(
+      "imu_yaw_buffer",
+      nh_ns.param<int>("imu_yaw_buffer", odomParams.imuYawBufferSize, 0.0));
+  readParam(
+      "whl_yaw_buffer",
+      nh_ns.param<int>("whl_yaw_buffer", odomParams.whlYawBufferSize, 0.0));
+  readParam("dist_buffer",
+            nh_ns.param<int>("dist_buffer", odomParams.distBufferSize, 0));
 }
 
-void OdometryNodelet::readParam(string paramName, bool result)
-{
-  if (!result)
-  {
+void OdometryNodelet::readParam(string paramName, bool result) {
+  if (!result) {
     ROS_FATAL("Param read failure. : %s", paramName.c_str());
   }
 }
 
-void OdometryNodelet::publishTransform(const nav_msgs::Odometry &odomMsg, std::string odomFrameName)
-{
+void OdometryNodelet::publishTransform(const nav_msgs::Odometry &odomMsg,
+                                       std::string odomFrameName) {
   tf::Transform odomTrans;
   tf::Pose tfPose;
 
   tf::poseMsgToTF(odomMsg.pose.pose, odomTrans);
-  odomBroadCaster->sendTransform(tf::StampedTransform(odomTrans, ros::Time::now(), odomFrameName, baselinkFrameName));
+  odomBroadCaster->sendTransform(tf::StampedTransform(
+      odomTrans, ros::Time::now(), odomFrameName, baselinkFrameName));
 }
 
-void OdometryNodelet::sensorCallback(const sensor_msgs::JointStateConstPtr &jointPos)
-{
+void OdometryNodelet::sensorCallback(
+    const sensor_msgs::JointStateConstPtr &jointPos) {
   double rRad, lRad, rVel, lVel;
   parseJointStatesMsg(jointPos, rRad, lRad, rVel, lVel);
   odometry.SetCurrentPosition(rRad, lRad, rVel, lVel);
   odometry.SetCurrentGyroZ(0.0);
 
   static double last_stamp = jointPos->header.stamp.toSec();
-  if (jointPos->header.stamp.toSec() - last_stamp > cycle_time - cycle_time / 10.0)
-  {
+  if (jointPos->header.stamp.toSec() - last_stamp >
+      cycle_time - cycle_time / 10.0) {
     double new_stamp = jointPos->header.stamp.toSec();
     double dT = new_stamp - last_stamp;
     last_stamp = new_stamp;
@@ -199,15 +237,17 @@ void OdometryNodelet::sensorCallback(const sensor_msgs::JointStateConstPtr &join
     // Published Whl Based Odometry.
     {
       nav_msgs::Odometry odomWhlMsg;
-      fillOdomMessage(odomWhlMsg, odomWhlFrameName, jointPos->header.stamp, odomWhl, velWhl);
+      fillOdomMessage(odomWhlMsg, odomWhlFrameName, jointPos->header.stamp,
+                      odomWhl, velWhl);
       publishTransform(odomWhlMsg, odomWhlFrameName);
       odomPubWhl.publish(odomWhlMsg);
     }
   }
 }
 
-void OdometryNodelet::sensorCallback(const kmsgs::ImuConstPtr &imuMsg, const sensor_msgs::JointStateConstPtr &jointPos)
-{
+void OdometryNodelet::sensorCallback(
+    const kmsgs::ImuConstPtr &imuMsg,
+    const sensor_msgs::JointStateConstPtr &jointPos) {
   double rRad, lRad, rVel, lVel;
   parseJointStatesMsg(jointPos, rRad, lRad, rVel, lVel);
   odometry.SetCurrentPosition(rRad, lRad, rVel, lVel);
@@ -215,34 +255,27 @@ void OdometryNodelet::sensorCallback(const kmsgs::ImuConstPtr &imuMsg, const sen
   calcAndPublishOdom(imuMsg->header.stamp);
 }
 
-void OdometryNodelet::parseJointStatesMsg(const sensor_msgs::JointStateConstPtr &jointPos, double &rRad, double &lRad,
-                                          double &rVel, double &lVel)
-{
-  for (int i = 0; i < jointPos->name.size(); i++)
-  {
+void OdometryNodelet::parseJointStatesMsg(
+    const sensor_msgs::JointStateConstPtr &jointPos, double &rRad, double &lRad,
+    double &rVel, double &lVel) {
+  for (int i = 0; i < jointPos->name.size(); i++) {
     string name = jointPos->name[i];
     double jntPos = jointPos->position[i];
     double jntVel = jointPos->velocity[i];
 
-    if (name == "right")
-    {
+    if (name == "right") {
       rRad = -jntPos;
       rVel = -jntVel;
-    }
-    else if (name == "left")
-    {
+    } else if (name == "left") {
       lRad = jntPos;
       lVel = jntVel;
-    }
-    else
-    {
+    } else {
       ROS_ERROR("Unexpected Joint Name : %s", name.c_str());
     }
   }
 }
 
-void OdometryNodelet::calcAndPublishOdom(const ros::Time &stamp)
-{
+void OdometryNodelet::calcAndPublishOdom(const ros::Time &stamp) {
   static double lastStamp = stamp.toSec();
   double newStamp = stamp.toSec();
   double dT = newStamp - lastStamp;
@@ -252,8 +285,7 @@ void OdometryNodelet::calcAndPublishOdom(const ros::Time &stamp)
   OdomVel velWhl, velImu;
   odometry.CalculateOdometry(odomWhl, odomImu, velWhl, velImu, dT);
 
-  if (!odom_whl_only)
-  {
+  if (!odom_whl_only) {
     // Published IMU based odometry.
     {
       nav_msgs::Odometry odomImuMsg;
@@ -272,9 +304,11 @@ void OdometryNodelet::calcAndPublishOdom(const ros::Time &stamp)
   }
 }
 
-void OdometryNodelet::fillOdomMessage(nav_msgs::Odometry &odomMsg, std::string odomFrameName, const ros::Time &stamp,
-                                      const OdomPos &odomPos, const OdomVel &odomVel)
-{
+void OdometryNodelet::fillOdomMessage(nav_msgs::Odometry &odomMsg,
+                                      std::string odomFrameName,
+                                      const ros::Time &stamp,
+                                      const OdomPos &odomPos,
+                                      const OdomVel &odomVel) {
   // Header
   odomMsg.header.frame_id = odomFrameName;
   odomMsg.child_frame_id = baselinkFrameName;
